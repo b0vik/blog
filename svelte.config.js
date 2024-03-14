d153af99de987fcccf023e95c7f8dfae9650a457
maximus maximus <maxwellleon55243@gmail.com>
2024-03-14 10:01:48 -0400

    so close...

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,10 @@ import { mdsvex, escapeSvelte } from 'mdsvex';
 import { getHighlighter } from 'shiki';
 import rehypeMathjax from 'rehype-mathjax';
 import remarkMath from 'remark-math';
+import fs from 'fs';
+
+// const gruvboxDark = JSON.parse(fs.readFileSync('src/themes/shiki/gruvbox-dark.json', 'utf8'));
+
 
 
 /** @type {import('mdsvex').MdsvexOptions} */
@@ -16,11 +20,12 @@ const mdsvexOptions = {
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
 			const highlighter = await getHighlighter({
-				themes: ['poimandres'],
+				themes: ['dark-plus'],
 				langs: ['javascript', 'typescript']
 			})
-			await highlighter.loadLanguage('javascript', 'typescript')
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			await highlighter.loadLanguage('javascript', 'typescript', 'shell', 'json', 'fish')
+			// await highlighter.loadTheme(gruvboxDark);
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'dark-plus' }))
 			return `{@html \`${html}\` }`
 		}
 	},
